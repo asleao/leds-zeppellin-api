551d630b477d3c017982d92cb1166c83ff987cde
André Lẽao <andre.sp.leao@gmail.com>
2017-12-21 17:17:03 -0200

    Alteração do nome das rotas de autenticação para accounts.

--- project/urls.py
+++ project/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('zeppelin.urls')),
     url(r'^oauth/', include('rest_framework_social_oauth2.urls')),
+    url(r'^', include('django.contrib.auth.urls')),
+    url(r'accounts/', include('rest_auth.urls')),
+    url(r'^accounts/registration/', include('rest_auth.registration.urls')),
 ]
--- zeppelin/urls.py
+++ zeppelin/urls.py
@@ -3,7 +3,5 @@ from django.conf.urls import include
 
 
 urlpatterns = [
-    url(r'^', include('django.contrib.auth.urls')),
-    url(r'auth/', include('rest_auth.urls')),
-    url(r'^auth/registration/', include('rest_auth.registration.urls')),
+
 ]
