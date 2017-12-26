1df3acfbffb1ca6e3b762f97bbefb43694aad6ba
André Lẽao <andre.sp.leao@gmail.com>
2017-12-25 22:07:09 -0200

    Refatoração dos serializers para criar os nested serilizers.
    Neste commit todos os serializers precisaram ser refatorados
    para que o serializer de projeto retornasse o json para cada
    objeto que ele tem relacionamento, ou seja, ao invés de mostrar
    apenas as pks de tools, team, languagem e owner, ele mostrasse
    o json destes objetos.
    
    Para que isso fosse possível os métodos create() de todos os serializers
    precisaram ser implementados por conta dos validadores que impediam
    de associar um objeto já existente no banco. Exemplo: se eu fizesse
    um post de um novo projeto com ferramentas que já existissem no banco
    o validador dizia q tais ferramentas já existiam e não criava o
    projeto. Com a retirada do validador e implementação do create() de cada
    serializer, para validar a existencia de um objeto com mesmos atributos
    no banco, foi possível realizar a criação dos projetos com a
    representação em JSON de todos os seus relacionamentos.

--- zeppelin/serializers.py
+++ zeppelin/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from . import models
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -8,21 +9,17 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('id', 'username')
+        extra_kwargs = {
+            'username': {
+                'validators': [UnicodeUsernameValidator()],
+            }
+        }
 
     def create(self, validated_data):
-        """Create and return a new user."""
-
-        user = User.objects.create(
-            username=validated_data['username'],
-            email=validated_data['email']
-        )
-
-        user.set_password(validated_data['password'])
-        user.save()
-
-        return user
+        if User.objects.filter(username=self.validated_data['username']).exists():
+            raise serializers.ValidationError("A User with this username already exists")
+        return User.objects.create(**validated_data)
 
 
 class ToolSerializer(serializers.ModelSerializer):
@@ -31,6 +28,16 @@ class ToolSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Tool
         fields = ('id', 'name')
+        extra_kwargs = {
+            'name': {
+                'validators': [],
+            }
+        }
+
+    def create(self, validated_data):
+        if models.Tool.objects.filter(name=self.validated_data['name']).exists():
+            raise serializers.ValidationError("A Tool with this name already exists")
+        return models.Tool.objects.create(**validated_data)
 
 
 class LanguageSerializer(serializers.ModelSerializer):
@@ -39,22 +46,50 @@ class LanguageSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Language
         fields = ('id', 'name')
+        extra_kwargs = {
+            'name': {
+                'validators': [],
+            }
+        }
+
+    def create(self, validated_data):
+        if models.Language.objects.filter(name=self.validated_data['name']).exists():
+            raise serializers.ValidationError("A Language with this name already exists")
+        return models.Language.objects.create(**validated_data)
 
 
 class ProjectSerializer(serializers.ModelSerializer):
     """A serializer for our Project objects."""
 
-    tools = serializers.SlugRelatedField(
-        many=True,
-        queryset=models.Tool.objects.all(),
-        slug_field='name'
-    )
-    team = serializers.SlugRelatedField(
-        many=True,
-        queryset=User.objects.all(),
-        slug_field='username'
-    )
+    tools = ToolSerializer(many=True)
+    team = UserSerializer(many=True)
+    owner = UserSerializer()
+    language = LanguageSerializer()
 
     class Meta:
         model = models.Project
         fields = ('id', 'name', 'tools', 'team', 'owner', 'language')
+
+    def create(self, validated_data):
+
+        tool_data = validated_data.pop('tools')
+        team_data = validated_data.pop('team')
+        owner_data = validated_data.pop('owner')
+        language_data = validated_data.pop('language')
+
+        owner = User.objects.get(username=owner_data['username'])
+        language = models.Language.objects.get(name=language_data['name'])
+
+        project = models.Project.objects.create(owner=owner, language=language, **validated_data)
+
+        for tool in tool_data:
+            tool = models.Tool.objects.get(name=tool['name'])
+            project.tools.add(tool.id)
+
+        for team in team_data:
+            team = User.objects.get(username=team['username'])
+            project.team.add(team.id)
+
+        project.save()
+
+        return project
