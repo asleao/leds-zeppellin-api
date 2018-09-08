5ee363f2932688353b0c559a18645ba0c97de88e
André Spadetto Leão <andre.sp.leao@gmail.com>
2018-09-08 11:39:38 -0300

    Updating endpoints for authorization

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/v1/', include('zeppelin.urls')),
-    url(r'^', include('django.contrib.auth.urls')),
-    url(r'accounts/', include('rest_auth.urls')),
-    url(r'^accounts/registration/', include('rest_auth.registration.urls')),
+    url(r'^api/v1/', include('zeppelin.urls')),    
+    url(r'^api/v1/registration/', include('rest_auth.registration.urls')),
+    url(r'^api/v1/token/', obtain_jwt_token),
 ]
