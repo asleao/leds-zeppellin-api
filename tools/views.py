804adcbc129dc2ee7fc1acdcfad431014a5f99f0
Andre Spadetto Leao <andre.sp.leao@gmail.com>
2019-01-22 22:04:25 -0200

    Factory boy and tools_without_credentials implementation.
    The method was implementated and tested.

--- tools/views.py
+++ tools/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from tools.models import ToolCredential
 
-# Create your views here.
+
+def tools_without_credentials(owner, tools):
+    tools_pending_credentials = []
+    for tool in tools:
+        has_credential = ToolCredential.objects.filter(owner=owner, tool=tool).exists()
+        if not has_credential:
+            tools_pending_credentials.append(tool)
+
+    return tools_pending_credentials
