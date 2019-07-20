322a973a9099a71750a1f3c21a7c5283187f6b14
Andre Spadetto Leao <andre.sp.leao@gmail.com>
2019-07-20 17:58:54 -0300

    Saving toolcredentials via api.

--- tools/api/viewsets.py
+++ tools/api/viewsets.py
@@ -1,9 +1,13 @@
+from django.contrib.auth.models import User
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from tools.api.serializers import ToolSerializer
-from tools.models import Tool
+from tools.models import Tool, ToolCredential
 
 
 class ToolViewSet(ModelViewSet):
@@ -12,3 +16,25 @@ class ToolViewSet(ModelViewSet):
     serializer_class = ToolSerializer
     permission_classes = (IsAuthenticated,)
     authentication_classes = (JWTAuthentication,)
+    lookup_field = 'id'
+
+    # TODO: finish validations
+    @action(methods=['post'], detail=True)
+    def credential(self, request, id):
+        owner = User.objects.get(pk=request.data['owner_id'])
+        username = request.data['username']
+        password = request.data['password']
+        token = request.data['token']
+
+        if not token and not username and not password:
+            return Response(status=status.HTTP_400_BAD_REQUEST,
+                            data={"detail": "A token or a username and password should be provided."})
+
+        toolcredential = ToolCredential(owner=owner,
+                                        tool_id=id,
+                                        username=username,
+                                        password=password,
+                                        token=token)
+        toolcredential.save()
+
+        return Response(status=status.HTTP_201_CREATED)
